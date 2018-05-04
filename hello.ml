088ec82a62f84c26126712cde271fa983ec9ab25
Feihong Hsu <feihong.hsu@gmail.com>
2018-05-03 20:07:19 -0500

    Add jbuild file

--- hello.ml
+++ hello.ml
@@ -1,10 +1,13 @@
-#require "core.top";;
-#require "core.syntax";;
 open Base
 open Core
 
+let get_random_hanzi () =
+  let buf = Buffer.create 1 in
+  let chr = Random.int_incl 0x4e00 0x9fff |> Uchar.of_int in
+  Buffer.add_utf_8_uchar buf chr;
+  Buffer.contents buf
+
 let () =
-    print_endline "你好世界！";
-    Random.self_init();
-    let ord = Random.int_incl 0x4e00 0x9fff in
-    printf "Number: %d\n" ord;
+  print_endline "你好世界！";
+  Random.self_init();
+  printf "Random hanzi: %s\n" get_random_hanzi();
