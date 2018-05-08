72f09229d2ad915577af35cfe10185b3992225ec
Feihong Hsu <feihong.hsu@gmail.com>
2018-05-08 16:03:18 -0500

    Write more content to the output file

--- write_file/write_file.ml
+++ write_file/write_file.ml
@@ -1,10 +1,19 @@
-(* open Core *)
+open Base
+open Lwt_io
 
+let get_random_hanzi () =
+  let buf = Buffer.create 1 in
+  let chr = Random.int_incl 0x4e00 0x9fff |> Uchar.of_scalar_exn in
+  Caml.Buffer.add_utf_8_uchar buf chr;
+  Buffer.contents buf
 
-Lwt_main.run begin
-  Lwt_io.with_file ~mode:Lwt_io.output "output.txt" begin fun channel ->
-    let%lwt () = Lwt_io.write channel "Hello there" in
-    let%lwt () = Lwt_io.write channel ", what nice weather we have today!" in
-    Lwt_io.printf "Wrote to file output.txt\n"
+let () =
+  Lwt_main.run begin
+    with_file ~mode:Lwt_io.output "output.txt" begin fun channel ->
+      let writeln content = write_line channel content in
+      let%lwt () = writeln "Do you know these random Chinese characters?" in
+      let%lwt () = writeln "Test your knowledge!\n" in
+      let%lwt () = writeln (get_random_hanzi ()) in
+      Lwt_io.printf "Wrote to file output.txt\n"
+    end
   end
-end
