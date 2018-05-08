33206c4342ba68ebbcea2892bc857c43a3eee46b
Feihong Hsu <feihong.hsu@gmail.com>
2018-05-08 16:50:48 -0500

    Refactor to use let%lwt syntax sugar
    instead of bind operators

--- http_client/client.ml
+++ http_client/client.ml
@@ -1,19 +1,18 @@
 (*
 https://github.com/mirage/ocaml-cohttp
 *)
-open Lwt
 open Cohttp
 open Cohttp_lwt_unix
 
-let body =
-  Client.get (Uri.of_string "http://ipecho.net/plain") >>= fun (resp, body) ->
+let content =
+  let%lwt resp, body = Client.get (Uri.of_string "http://ipecho.net/plain") in
   let code = resp |> Response.status |> Code.code_of_status in
   Printf.printf "Response code: %d\n" code;
-  Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
-  body |> Cohttp_lwt.Body.to_string >|= fun body ->
-  Printf.printf "Body of length: %d\n" (String.length body);
-  body
+  Printf.printf "Headers: \n%s\n" (resp |> Response.headers |> Header.to_string);
+  let%lwt content = Cohttp_lwt.Body.to_string body in
+  Printf.printf "Body of length: %d\n" (String.length content);
+  Lwt.return content
 
 let () =
-  let body = Lwt_main.run body in
-  print_endline ("Received body\n" ^ body)
+  let content = Lwt_main.run content in
+  print_endline ("Received content:\n" ^ content)
