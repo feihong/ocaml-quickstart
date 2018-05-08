7c7018fb60fc94a1e76ae7d3679a38695de6f6b4
Feihong Hsu <feihong.hsu@gmail.com>
2018-05-08 15:53:47 -0500

    Fix jbuild so that you can use let%lwt syntax sugar

--- write_file/write_file.ml
+++ write_file/write_file.ml
@@ -1,7 +1,10 @@
 (* open Core *)
 
+
 Lwt_main.run begin
   Lwt_io.with_file ~mode:Lwt_io.output "output.txt" begin fun channel ->
-    Lwt_io.write channel "Hello there"
+    let%lwt () = Lwt_io.write channel "Hello there" in
+    let%lwt () = Lwt_io.write channel ", what nice weather we have today!" in
+    Lwt_io.printf "Wrote to file output.txt\n"
   end
 end
