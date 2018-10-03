46d02cf43cff7e80681011c35e76a0ddf882935c
Feihong Hsu <feihong.hsu@gmail.com>
2018-10-02 23:24:29 -0700

    Print the number of bytes for each hanzi

--- hello.ml
+++ hello.ml
@@ -1,14 +1,15 @@
-open Stdio
+open Printf
 
-let get_random_hanzi () =
+let print_random_hanzi () =
   let buf = Buffer.create 1 in
   let chr = Base.Random.int_incl 0x4e00 0x9fff |> Uchar.of_int in
   Buffer.add_utf_8_uchar buf chr;
-  Buffer.contents buf
+  let len = buf |> Buffer.length in
+  printf "Random hanzi: %s (%d bytes)\n" (buf |> Buffer.contents) len
 
 let () =
   let () = Base.Random.self_init() in
   print_endline "你好世界！";
-  for _ = 1 to 8 do
-    print_endline ("Random hanzi: " ^ get_random_hanzi())
+  for _ = 1 to 5 do
+    print_random_hanzi ()
   done
